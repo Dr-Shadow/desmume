Dr-Shadow/desmume

commit 313cf21116611d096b53ea390b8fbbf860b4ab8e

core: - refactoring for new boot code;
git-svn-id: svn://svn.code.sf.net/p/desmume/code/trunk/desmume@4685 5577840e-afb3-4e59-9ef3-2b7f898b1952

src/encrypt.h
@@ -1,0 +1,64 @@
+/*
+	Copyright (C) 2009-2013 DeSmuME team
+
+	This file is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 2 of the License, or
+	(at your option) any later version.
+
+	This file is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with the this software.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef _ENCRYPT_H_
+#define _ENCRYPT_H_
+#include "types.h"
+
+struct _KEY1
+{
+	_KEY1(u8 *keyBufPtr)
+	{
+		if (keyBuf) delete keyBuf;
+		keyBuf = new u32 [0x1048 / sizeof(u32)];
+		memset(keyBuf, 0x00, 0x1048);
+		memset(&keyCode[0], 0, sizeof(keyCode));
+		this->keyBufPtr = keyBufPtr;
+	}
+
+	~_KEY1()
+	{
+		if (keyBuf) 
+		{
+			delete keyBuf;
+			keyBuf = NULL;
+		}
+	}
+
+	u32 *keyBuf;
+	u32 keyCode[3];
+	u8	*keyBufPtr;
+
+	void init(u32 idcode, u8 level, u8 modulo);
+	void applyKeycode(u8 modulo);
+	void decrypt(u32 *ptr);
+	void encrypt(u32 *ptr);
+};
+
+struct _KEY2
+{
+	_KEY2() :	seed0(0x58C56DE0E8U), 
+					seed1(0x5C879B9B05U)
+	{}
+
+	u64 seed0;
+	u64 seed1;
+
+	void applySeed(u8 PROCNUM);
+};
+
+#endif

src/Makefile.am
@@ -19,7 +19,7 @@ libdesmume_a_SOURCES = \
 	common.cpp common.h \
 	debug.cpp debug.h \
 	Disassembler.cpp Disassembler.h \
-	emufile.h emufile.cpp emufile_types.h FIFO.cpp FIFO.h \
+	emufile.h emufile.cpp emufile_types.h encrypt.h encrypt.cpp FIFO.cpp FIFO.h \
 	firmware.cpp firmware.h GPU.cpp GPU.h \
 	fs.h \
 	GPU_osd.h \

src/encrypt.cpp
@@ -1,0 +1,111 @@
+/*
+	Copyright (C) 2009-2013 DeSmuME team
+
+	This file is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 2 of the License, or
+	(at your option) any later version.
+
+	This file is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with the this software.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "encrypt.h"
+#include "MMU.h"
+
+//================================================================================== KEY1
+#define bswap32(val) (((val & 0x000000FF) << 24) | ((val & 0x0000FF00) << 8) | ((val & 0x00FF0000) >> 8) | ((val & 0xFF000000) >> 24))
+#define DWNUM(i) ((i) >> 2)
+
+void _KEY1::init(u32 idcode, u8 level, u8 modulo)
+{
+	memcpy(keyBuf, keyBufPtr, 0x1048);
+	keyCode[0] = idcode;
+	keyCode[1] = idcode >> 1;
+	keyCode[2] = idcode << 1;
+	if (level >= 1)				// first apply (always)
+		applyKeycode(modulo);
+	if (level >= 2)				// second apply (optional)
+		applyKeycode(modulo);
+	keyCode[1] <<= 1;
+	keyCode[2] >>= 1;
+	if (level >= 3)				// third apply (optional)
+		applyKeycode(modulo);
+
+	printf("keycode1: %08X%08X%08X\n", keyCode[2], keyCode[1], keyCode[0]);
+}
+
+void _KEY1::applyKeycode(u8 modulo)
+{
+	encrypt(&keyCode[1]);
+	encrypt(&keyCode[0]);
+	
+	u32 scratch[2] = {0};
+
+	for (u32 i = 0; i <= 0x44; i += 4)			// xor with reversed byte-order (bswap)
+		keyBuf[DWNUM(i)] ^= bswap32(keyCode[DWNUM(i % modulo)]);
+
+	for (u32 i = 0; i <= 0x1040; i += 8)
+	{
+		encrypt(scratch);						// encrypt S (64bit) by keybuf
+		keyBuf[DWNUM(i)] = scratch[1];			// write S to keybuf (first upper 32bit)
+		keyBuf[DWNUM(i+4)] = scratch[0];		// write S to keybuf (then lower 32bit)
+	}
+}
+
+void _KEY1::decrypt(u32 *ptr)
+{
+	u32 y = ptr[0];
+	u32 x = ptr[1];
+
+	for (u32 i = 0x11; i >= 0x02; i--)
+	{
+		u32 z = keyBuf[i] ^ x;
+		x = keyBuf[DWNUM(0x048 + (((z >> 24) & 0xFF) << 2))];
+		x = keyBuf[DWNUM(0x448 + (((z >> 16) & 0xFF) << 2))] + x;
+		x = keyBuf[DWNUM(0x848 + (((z >>  8) & 0xFF) << 2))] ^ x;
+		x = keyBuf[DWNUM(0xC48 + (((z >>  0) & 0xFF) << 2))] + x;
+		x = y ^ x;
+		y = z;
+	}
+	ptr[0] = x ^ keyBuf[DWNUM(0x04)];
+	ptr[1] = y ^ keyBuf[DWNUM(0x00)];
+}
+
+void _KEY1::encrypt(u32 *ptr)
+{
+	u32 y = ptr[0];
+	u32 x = ptr[1];
+
+	for (u32 i = 0x00; i <= 0x0F; i++)
+	{
+		u32 z = keyBuf[i] ^ x;
+		x = keyBuf[DWNUM(0x048 + (((z >> 24) & 0xFF) << 2))];
+		x = keyBuf[DWNUM(0x448 + (((z >> 16) & 0xFF) << 2))] + x;
+		x = keyBuf[DWNUM(0x848 + (((z >>  8) & 0xFF) << 2))] ^ x;
+		x = keyBuf[DWNUM(0xC48 + (((z >>  0) & 0xFF) << 2))] + x;
+		x = y ^ x;
+		y = z;
+	}
+
+	ptr[0] = x ^ keyBuf[DWNUM(0x40)];
+	ptr[1] = y ^ keyBuf[DWNUM(0x44)];
+}
+#undef DWNUM
+#undef bswap32
+
+//================================================================================== KEY2
+void _KEY2::applySeed(u8 PROCNUM)
+{
+	u64 tmp = (MMU_read8(PROCNUM, REG_ENCSEED0H) & 0xFF);
+	seed0 = MMU_read32(PROCNUM, REG_ENCSEED0L) | (tmp << 32);
+	tmp = (MMU_read8(PROCNUM, REG_ENCSEED1H) & 0xFF);
+	seed1 = MMU_read32(PROCNUM, REG_ENCSEED1L) | (tmp << 32);
+	printf("ARM%c: set KEY2 seed0 to %010llX\n", PROCNUM?'7':'9', seed0);
+	printf("ARM%c: set KEY2 seed1 to %010llX\n", PROCNUM?'7':'9', seed1);
+}

commit 23d4b9ffb98d5601dc1d8980853b21c156e2a5f9

Cocoa Port: - Fix Cocoa port builds. (Regression from r4685.)
git-svn-id: svn://svn.code.sf.net/p/desmume/code/trunk/desmume@4687 5577840e-afb3-4e59-9ef3-2b7f898b1952

src/cocoa/DeSmuME (Legacy).xcodeproj/project.pbxproj
@@ -764,6 +764,13 @@
 		AB46781814ABD4890002FF94 /* AppIcon_ROMSave.icns in Resources */ = {isa = PBXBuildFile; fileRef = AB46780814ABD4890002FF94 /* AppIcon_ROMSave.icns */; };
 		AB46781914ABD4890002FF94 /* AppIcon_SaveState.icns in Resources */ = {isa = PBXBuildFile; fileRef = AB46780914ABD4890002FF94 /* AppIcon_SaveState.icns */; };
 		AB4F981716C30117000E90EA /* OGLRender.cpp in Sources */ = {isa = PBXBuildFile; fileRef = AB06CD2E135B8ACE00E977B3 /* OGLRender.cpp */; };
+		AB52F9EB178CA82200FD6E89 /* encrypt.cpp in Sources */ = {isa = PBXBuildFile; fileRef = AB52F9EA178CA82200FD6E89 /* encrypt.cpp */; };
+		AB52F9EC178CA82200FD6E89 /* encrypt.cpp in Sources */ = {isa = PBXBuildFile; fileRef = AB52F9EA178CA82200FD6E89 /* encrypt.cpp */; };
+		AB52F9ED178CA82200FD6E89 /* encrypt.cpp in Sources */ = {isa = PBXBuildFile; fileRef = AB52F9EA178CA82200FD6E89 /* encrypt.cpp */; };
+		AB52F9EE178CA82200FD6E89 /* encrypt.cpp in Sources */ = {isa = PBXBuildFile; fileRef = AB52F9EA178CA82200FD6E89 /* encrypt.cpp */; };
+		AB52F9EF178CA82200FD6E89 /* encrypt.cpp in Sources */ = {isa = PBXBuildFile; fileRef = AB52F9EA178CA82200FD6E89 /* encrypt.cpp */; };
+		AB52F9F0178CA82200FD6E89 /* encrypt.cpp in Sources */ = {isa = PBXBuildFile; fileRef = AB52F9EA178CA82200FD6E89 /* encrypt.cpp */; };
+		AB52F9F1178CA82200FD6E89 /* encrypt.cpp in Sources */ = {isa = PBXBuildFile; fileRef = AB52F9EA178CA82200FD6E89 /* encrypt.cpp */; };
 		AB73B12B14BDA01C00F49C92 /* appDelegate_legacy.mm in Sources */ = {isa = PBXBuildFile; fileRef = AB73B12914BDA01C00F49C92 /* appDelegate_legacy.mm */; };
 		AB73B12C14BDA01C00F49C92 /* emuWindowDelegate_legacy.mm in Sources */ = {isa = PBXBuildFile; fileRef = AB73B12A14BDA01C00F49C92 /* emuWindowDelegate_legacy.mm */; };
 		AB73B12D14BDA01C00F49C92 /* appDelegate_legacy.mm in Sources */ = {isa = PBXBuildFile; fileRef = AB73B12914BDA01C00F49C92 /* appDelegate_legacy.mm */; };
@@ -1324,6 +1331,8 @@
 		AB46780714ABD4890002FF94 /* AppIcon_NintendoDS_ROM.icns */ = {isa = PBXFileReference; lastKnownFileType = image.icns; path = AppIcon_NintendoDS_ROM.icns; sourceTree = "<group>"; };
 		AB46780814ABD4890002FF94 /* AppIcon_ROMSave.icns */ = {isa = PBXFileReference; lastKnownFileType = image.icns; path = AppIcon_ROMSave.icns; sourceTree = "<group>"; };
 		AB46780914ABD4890002FF94 /* AppIcon_SaveState.icns */ = {isa = PBXFileReference; lastKnownFileType = image.icns; path = AppIcon_SaveState.icns; sourceTree = "<group>"; };
+		AB52F9E9178CA82200FD6E89 /* encrypt.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; name = encrypt.h; path = ../encrypt.h; sourceTree = SOURCE_ROOT; };
+		AB52F9EA178CA82200FD6E89 /* encrypt.cpp */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.cpp.cpp; name = encrypt.cpp; path = ../encrypt.cpp; sourceTree = SOURCE_ROOT; };
 		AB73B12714BDA01C00F49C92 /* appDelegate_legacy.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; name = appDelegate_legacy.h; path = userinterface/appDelegate_legacy.h; sourceTree = "<group>"; };
 		AB73B12814BDA01C00F49C92 /* emuWindowDelegate_legacy.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; name = emuWindowDelegate_legacy.h; path = userinterface/emuWindowDelegate_legacy.h; sourceTree = "<group>"; };
 		AB73B12914BDA01C00F49C92 /* appDelegate_legacy.mm */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.cpp.objcpp; name = appDelegate_legacy.mm; path = userinterface/appDelegate_legacy.mm; sourceTree = "<group>"; };
@@ -1650,6 +1659,7 @@
 				AB06CD1A135B8ACE00E977B3 /* Disassembler.cpp */,
 				AB06CD1B135B8ACE00E977B3 /* driver.cpp */,
 				AB06CD1C135B8ACE00E977B3 /* emufile.cpp */,
+				AB52F9EA178CA82200FD6E89 /* encrypt.cpp */,
 				AB06CD1D135B8ACE00E977B3 /* FIFO.cpp */,
 				AB06CD1E135B8ACE00E977B3 /* firmware.cpp */,
 				AB06CD1F135B8ACE00E977B3 /* fs-linux.cpp */,
@@ -1699,6 +1709,7 @@
 				AB06CCE7135B8ACE00E977B3 /* driver.h */,
 				AB06CCE9135B8ACE00E977B3 /* emufile.h */,
 				AB06CCE8135B8ACE00E977B3 /* emufile_types.h */,
+				AB52F9E9178CA82200FD6E89 /* encrypt.h */,
 				AB06CCEA135B8ACE00E977B3 /* FIFO.h */,
 				AB06CCEB135B8ACE00E977B3 /* firmware.h */,
 				AB06CCEC135B8ACE00E977B3 /* fs.h */,
@@ -2871,6 +2882,7 @@
 				ABF50C04169F61540018C08D /* x86operand.cpp in Sources */,
 				ABF50C05169F61540018C08D /* x86util.cpp in Sources */,
 				AB2F5B051704EE0100E28885 /* utilities.c in Sources */,
+				AB52F9F0178CA82200FD6E89 /* encrypt.cpp in Sources */,
 			);
 			runOnlyForDeploymentPostprocessing = 0;
 		};
@@ -3012,6 +3024,7 @@
 				ABF50C5B169F61540018C08D /* x86operand.cpp in Sources */,
 				ABF50C5C169F61540018C08D /* x86util.cpp in Sources */,
 				AB2F5B081704EE0100E28885 /* utilities.c in Sources */,
+				AB52F9EE178CA82200FD6E89 /* encrypt.cpp in Sources */,
 			);
 			runOnlyForDeploymentPostprocessing = 0;
 		};
@@ -3153,6 +3166,7 @@
 				ABF50BCA169F61540018C08D /* x86operand.cpp in Sources */,
 				ABF50BCB169F61540018C08D /* x86util.cpp in Sources */,
 				AB2F5B031704EE0100E28885 /* utilities.c in Sources */,
+				AB52F9EC178CA82200FD6E89 /* encrypt.cpp in Sources */,
 			);
 			runOnlyForDeploymentPostprocessing = 0;
 		};
@@ -3264,6 +3278,7 @@
 				AB18153E15D212B4007A6CC3 /* ringbuffer.cpp in Sources */,
 				AB1816B615D216EC007A6CC3 /* OGLRender.cpp in Sources */,
 				AB2F5B071704EE0100E28885 /* utilities.c in Sources */,
+				AB52F9EB178CA82200FD6E89 /* encrypt.cpp in Sources */,
 			);
 			runOnlyForDeploymentPostprocessing = 0;
 		};
@@ -3375,6 +3390,7 @@
 				AB1815DA15D21469007A6CC3 /* ringbuffer.cpp in Sources */,
 				AB4F981716C30117000E90EA /* OGLRender.cpp in Sources */,
 				AB2F5B091704EE0100E28885 /* utilities.c in Sources */,
+				AB52F9ED178CA82200FD6E89 /* encrypt.cpp in Sources */,
 			);
 			runOnlyForDeploymentPostprocessing = 0;
 		};
@@ -3496,6 +3512,7 @@
 				AB18168015D214F2007A6CC3 /* ringbuffer.cpp in Sources */,
 				AB1816B815D216F5007A6CC3 /* OGLRender.cpp in Sources */,
 				AB2F5B041704EE0100E28885 /* utilities.c in Sources */,
+				AB52F9F1178CA82200FD6E89 /* encrypt.cpp in Sources */,
 			);
 			runOnlyForDeploymentPostprocessing = 0;
 		};
@@ -3637,6 +3654,7 @@
 				ABF50C21169F61540018C08D /* x86operand.cpp in Sources */,
 				ABF50C22169F61540018C08D /* x86util.cpp in Sources */,
 				AB2F5B061704EE0100E28885 /* utilities.c in Sources */,
+				AB52F9EF178CA82200FD6E89 /* encrypt.cpp in Sources */,
 			);
 			runOnlyForDeploymentPostprocessing = 0;
 		};

src/cocoa/DeSmuME (XCode 3).xcodeproj/project.pbxproj
@@ -830,6 +830,11 @@
 		ABA03571169127C000817C69 /* troubleshootingWindowDelegate.mm in Sources */ = {isa = PBXBuildFile; fileRef = ABA0356F169127C000817C69 /* troubleshootingWindowDelegate.mm */; };
 		ABA03572169127C000817C69 /* troubleshootingWindowDelegate.mm in Sources */ = {isa = PBXBuildFile; fileRef = ABA0356F169127C000817C69 /* troubleshootingWindowDelegate.mm */; };
 		ABA03573169127C000817C69 /* troubleshootingWindowDelegate.mm in Sources */ = {isa = PBXBuildFile; fileRef = ABA0356F169127C000817C69 /* troubleshootingWindowDelegate.mm */; };
+		ABA93EB0178C7D4200CBF50B /* encrypt.cpp in Sources */ = {isa = PBXBuildFile; fileRef = ABA93EAF178C7D4200CBF50B /* encrypt.cpp */; };
+		ABA93EB1178C7D4200CBF50B /* encrypt.cpp in Sources */ = {isa = PBXBuildFile; fileRef = ABA93EAF178C7D4200CBF50B /* encrypt.cpp */; };
+		ABA93EB2178C7D4200CBF50B /* encrypt.cpp in Sources */ = {isa = PBXBuildFile; fileRef = ABA93EAF178C7D4200CBF50B /* encrypt.cpp */; };
+		ABA93EB3178C7D4200CBF50B /* encrypt.cpp in Sources */ = {isa = PBXBuildFile; fileRef = ABA93EAF178C7D4200CBF50B /* encrypt.cpp */; };
+		ABA93EB4178C7D4200CBF50B /* encrypt.cpp in Sources */ = {isa = PBXBuildFile; fileRef = ABA93EAF178C7D4200CBF50B /* encrypt.cpp */; };
 		ABAAFBEA172122B6005DDDBE /* FileMigrationDelegate.mm in Sources */ = {isa = PBXBuildFile; fileRef = ABAAFBE9172122B6005DDDBE /* FileMigrationDelegate.mm */; };
 		ABAAFBEB172122B6005DDDBE /* FileMigrationDelegate.mm in Sources */ = {isa = PBXBuildFile; fileRef = ABAAFBE9172122B6005DDDBE /* FileMigrationDelegate.mm */; };
 		ABAAFBEC172122B6005DDDBE /* FileMigrationDelegate.mm in Sources */ = {isa = PBXBuildFile; fileRef = ABAAFBE9172122B6005DDDBE /* FileMigrationDelegate.mm */; };
@@ -1229,6 +1234,8 @@
 		ABA0356F169127C000817C69 /* troubleshootingWindowDelegate.mm */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.cpp.objcpp; path = troubleshootingWindowDelegate.mm; sourceTree = "<group>"; };
 		ABA6574914511EC90077E5E9 /* cocoa_cheat.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = cocoa_cheat.h; sourceTree = "<group>"; };
 		ABA6574A14511EC90077E5E9 /* cocoa_cheat.mm */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.cpp.objcpp; path = cocoa_cheat.mm; sourceTree = "<group>"; };
+		ABA93EAE178C7D4200CBF50B /* encrypt.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; name = encrypt.h; path = ../encrypt.h; sourceTree = SOURCE_ROOT; };
+		ABA93EAF178C7D4200CBF50B /* encrypt.cpp */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.cpp.cpp; name = encrypt.cpp; path = ../encrypt.cpp; sourceTree = SOURCE_ROOT; };
 		ABAAFBE8172122B6005DDDBE /* FileMigrationDelegate.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = FileMigrationDelegate.h; sourceTree = "<group>"; };
 		ABAAFBE9172122B6005DDDBE /* FileMigrationDelegate.mm */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.cpp.objcpp; path = FileMigrationDelegate.mm; sourceTree = "<group>"; };
 		ABAD104915ACE7A00000EC47 /* DeSmuME (PPC).app */ = {isa = PBXFileReference; explicitFileType = wrapper.application; includeInIndex = 0; path = "DeSmuME (PPC).app"; sourceTree = BUILT_PRODUCTS_DIR; };
@@ -1886,6 +1893,7 @@
 				ABD1FEAD1345AC8400AF11D1 /* Disassembler.cpp */,
 				ABD1FEAE1345AC8400AF11D1 /* driver.cpp */,
 				ABD1FEAF1345AC8400AF11D1 /* emufile.cpp */,
+				ABA93EAF178C7D4200CBF50B /* encrypt.cpp */,
 				ABD1FEB01345AC8400AF11D1 /* FIFO.cpp */,
 				ABD1FEB11345AC8400AF11D1 /* firmware.cpp */,
 				ABD1FEB21345AC8400AF11D1 /* fs-linux.cpp */,
@@ -1935,6 +1943,7 @@
 				ABD1FE7A1345AC8400AF11D1 /* driver.h */,
 				ABD1FE7C1345AC8400AF11D1 /* emufile.h */,
 				ABD1FE7B1345AC8400AF11D1 /* emufile_types.h */,
+				ABA93EAE178C7D4200CBF50B /* encrypt.h */,
 				ABD1FE7D1345AC8400AF11D1 /* FIFO.h */,
 				ABD1FE7E1345AC8400AF11D1 /* firmware.h */,
 				ABD1FE7F1345AC8400AF11D1 /* fs.h */,
@@ -3050,6 +3059,7 @@
 				AB2A9A691725F00F0062C1A1 /* InputProfileController.mm in Sources */,
 				AB2A9A6A1725F00F0062C1A1 /* audiosamplegenerator.cpp in Sources */,
 				AB2A9A6B1725F00F0062C1A1 /* FileMigrationDelegate.mm in Sources */,
+				ABA93EB4178C7D4200CBF50B /* encrypt.cpp in Sources */,
 			);
 			runOnlyForDeploymentPostprocessing = 0;
 		};
@@ -3180,6 +3190,7 @@
 				AB213D46170CB141006DDB0F /* InputProfileController.mm in Sources */,
 				AB2145241714DFF4006DDB0F /* audiosamplegenerator.cpp in Sources */,
 				ABAAFBEB172122B6005DDDBE /* FileMigrationDelegate.mm in Sources */,
+				ABA93EB1178C7D4200CBF50B /* encrypt.cpp in Sources */,
 			);
 			runOnlyForDeploymentPostprocessing = 0;
 		};
@@ -3340,6 +3351,7 @@
 				AB213D48170CB141006DDB0F /* InputProfileController.mm in Sources */,
 				AB2145261714DFF4006DDB0F /* audiosamplegenerator.cpp in Sources */,
 				ABAAFBEA172122B6005DDDBE /* FileMigrationDelegate.mm in Sources */,
+				ABA93EB0178C7D4200CBF50B /* encrypt.cpp in Sources */,
 			);
 			runOnlyForDeploymentPostprocessing = 0;
 		};
@@ -3500,6 +3512,7 @@
 				AB213D45170CB141006DDB0F /* InputProfileController.mm in Sources */,
 				AB2145251714DFF4006DDB0F /* audiosamplegenerator.cpp in Sources */,
 				ABAAFBED172122B6005DDDBE /* FileMigrationDelegate.mm in Sources */,
+				ABA93EB3178C7D4200CBF50B /* encrypt.cpp in Sources */,
 			);
 			runOnlyForDeploymentPostprocessing = 0;
 		};
@@ -3630,6 +3643,7 @@
 				AB213D47170CB141006DDB0F /* InputProfileController.mm in Sources */,
 				AB2145231714DFF4006DDB0F /* audiosamplegenerator.cpp in Sources */,
 				ABAAFBEC172122B6005DDDBE /* FileMigrationDelegate.mm in Sources */,
+				ABA93EB2178C7D4200CBF50B /* encrypt.cpp in Sources */,
 			);
 			runOnlyForDeploymentPostprocessing = 0;
 		};

src/cocoa/DeSmuME (XCode 4).xcodeproj/project.pbxproj
@@ -612,6 +612,9 @@
 		ABD1FF9F1345ACFA00AF11D1 /* metaspu.cpp in Sources */ = {isa = PBXBuildFile; fileRef = ABD1FF771345ACFA00AF11D1 /* metaspu.cpp */; };
 		ABD42047172319D1006A9B46 /* FileMigrationDelegate.mm in Sources */ = {isa = PBXBuildFile; fileRef = ABD42046172319D1006A9B46 /* FileMigrationDelegate.mm */; };
 		ABD42048172319D1006A9B46 /* FileMigrationDelegate.mm in Sources */ = {isa = PBXBuildFile; fileRef = ABD42046172319D1006A9B46 /* FileMigrationDelegate.mm */; };
+		ABD798C8178C7B9000089F19 /* encrypt.cpp in Sources */ = {isa = PBXBuildFile; fileRef = ABD798C7178C7B9000089F19 /* encrypt.cpp */; };
+		ABD798C9178C7B9000089F19 /* encrypt.cpp in Sources */ = {isa = PBXBuildFile; fileRef = ABD798C7178C7B9000089F19 /* encrypt.cpp */; };
+		ABD798CA178C7B9000089F19 /* encrypt.cpp in Sources */ = {isa = PBXBuildFile; fileRef = ABD798C7178C7B9000089F19 /* encrypt.cpp */; };
 		ABE5DE95143F781900835AD8 /* videofilter.cpp in Sources */ = {isa = PBXBuildFile; fileRef = AB817A35143EE2DB00A7DFE9 /* videofilter.cpp */; };
 		ABE5DFE5143FB1DA00835AD8 /* cocoa_videofilter.mm in Sources */ = {isa = PBXBuildFile; fileRef = ABE5DFE4143FB1DA00835AD8 /* cocoa_videofilter.mm */; };
 		ABE6702B1415DE6C00E8E4C9 /* tinystr.cpp in Sources */ = {isa = PBXBuildFile; fileRef = ABE670251415DE6C00E8E4C9 /* tinystr.cpp */; };
@@ -1049,6 +1052,8 @@
 		ABD1FF9B1345ACFA00AF11D1 /* Timestretcher.cpp */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.cpp.cpp; path = Timestretcher.cpp; sourceTree = "<group>"; };
 		ABD42045172319D1006A9B46 /* FileMigrationDelegate.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = FileMigrationDelegate.h; sourceTree = "<group>"; };
 		ABD42046172319D1006A9B46 /* FileMigrationDelegate.mm */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.cpp.objcpp; path = FileMigrationDelegate.mm; sourceTree = "<group>"; };
+		ABD798C6178C7B9000089F19 /* encrypt.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; name = encrypt.h; path = ../encrypt.h; sourceTree = "<group>"; };
+		ABD798C7178C7B9000089F19 /* encrypt.cpp */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.cpp.cpp; name = encrypt.cpp; path = ../encrypt.cpp; sourceTree = "<group>"; };
 		ABE5DFE3143FB1DA00835AD8 /* cocoa_videofilter.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = cocoa_videofilter.h; sourceTree = "<group>"; };
 		ABE5DFE4143FB1DA00835AD8 /* cocoa_videofilter.mm */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.cpp.objcpp; path = cocoa_videofilter.mm; sourceTree = "<group>"; };
 		ABE670251415DE6C00E8E4C9 /* tinystr.cpp */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.cpp.cpp; path = tinystr.cpp; sourceTree = "<group>"; };
@@ -1495,6 +1500,7 @@
 				ABD1FEAD1345AC8400AF11D1 /* Disassembler.cpp */,
 				ABD1FEAE1345AC8400AF11D1 /* driver.cpp */,
 				ABD1FEAF1345AC8400AF11D1 /* emufile.cpp */,
+				ABD798C7178C7B9000089F19 /* encrypt.cpp */,
 				ABD1FEB01345AC8400AF11D1 /* FIFO.cpp */,
 				ABD1FEB11345AC8400AF11D1 /* firmware.cpp */,
 				ABD1FEB21345AC8400AF11D1 /* fs-linux.cpp */,
@@ -1545,6 +1551,7 @@
 				ABD1FE7A1345AC8400AF11D1 /* driver.h */,
 				ABD1FE7C1345AC8400AF11D1 /* emufile.h */,
 				ABD1FE7B1345AC8400AF11D1 /* emufile_types.h */,
+				ABD798C6178C7B9000089F19 /* encrypt.h */,
 				ABD1FE7D1345AC8400AF11D1 /* FIFO.h */,
 				ABD1FE7E1345AC8400AF11D1 /* firmware.h */,
 				ABD1FE7F1345AC8400AF11D1 /* fs.h */,
@@ -2286,6 +2293,7 @@
 				ABD10AE81715FCDD00B5729D /* audiosamplegenerator.cpp in Sources */,
 				ABD10AEB1715FCDD00B5729D /* mic_ext.cpp in Sources */,
 				ABD42048172319D1006A9B46 /* FileMigrationDelegate.mm in Sources */,
+				ABD798C9178C7B9000089F19 /* encrypt.cpp in Sources */,
 			);
 			runOnlyForDeploymentPostprocessing = 0;
 		};
@@ -2447,6 +2455,7 @@
 				ABD10AE71715FCDD00B5729D /* audiosamplegenerator.cpp in Sources */,
 				ABD10AEA1715FCDD00B5729D /* mic_ext.cpp in Sources */,
 				ABD42047172319D1006A9B46 /* FileMigrationDelegate.mm in Sources */,
+				ABD798C8178C7B9000089F19 /* encrypt.cpp in Sources */,
 			);
 			runOnlyForDeploymentPostprocessing = 0;
 		};
@@ -2597,6 +2606,7 @@
 				ABD10AEE17160CDD00B5729D /* cocoa_input.mm in Sources */,
 				AB5785F817175F16002C5FC7 /* cocoa_core.mm in Sources */,
 				AB5785F9171760D2002C5FC7 /* cocoa_output.mm in Sources */,
+				ABD798CA178C7B9000089F19 /* encrypt.cpp in Sources */,
 			);
 			runOnlyForDeploymentPostprocessing = 0;
 		};

src/cocoa/DeSmuME (Xcode 5).xcodeproj/project.pbxproj
@@ -529,6 +529,9 @@
 		ABC5720D1344346600E7B0B1 /* AppKit.framework in Frameworks */ = {isa = PBXBuildFile; fileRef = 29B97324FDCFA39411CA2CEA /* AppKit.framework */; };
 		ABC572101344347000E7B0B1 /* Foundation.framework in Frameworks */ = {isa = PBXBuildFile; fileRef = 29B97325FDCFA39411CA2CEA /* Foundation.framework */; };
 		ABC719E2138CB25E002827A9 /* DefaultKeyMappings.plist in Resources */ = {isa = PBXBuildFile; fileRef = ABC719E1138CB25E002827A9 /* DefaultKeyMappings.plist */; };
+		ABCFA9F4178BDE920030C8BA /* encrypt.cpp in Sources */ = {isa = PBXBuildFile; fileRef = ABCFA9F3178BDE920030C8BA /* encrypt.cpp */; };
+		ABCFA9F5178BDE920030C8BA /* encrypt.cpp in Sources */ = {isa = PBXBuildFile; fileRef = ABCFA9F3178BDE920030C8BA /* encrypt.cpp */; };
+		ABCFA9F6178BDE920030C8BA /* encrypt.cpp in Sources */ = {isa = PBXBuildFile; fileRef = ABCFA9F3178BDE920030C8BA /* encrypt.cpp */; };
 		ABD1041C1346652500AF11D1 /* cocoa_input.mm in Sources */ = {isa = PBXBuildFile; fileRef = ABD104111346652500AF11D1 /* cocoa_input.mm */; };
 		ABD1041D1346652500AF11D1 /* cocoa_core.mm in Sources */ = {isa = PBXBuildFile; fileRef = ABD104121346652500AF11D1 /* cocoa_core.mm */; };
 		ABD1041E1346652500AF11D1 /* cocoa_rom.mm in Sources */ = {isa = PBXBuildFile; fileRef = ABD104131346652500AF11D1 /* cocoa_rom.mm */; };
@@ -866,6 +869,8 @@
 		ABC570D0134431CE00E7B0B1 /* AudioUnit.framework */ = {isa = PBXFileReference; lastKnownFileType = wrapper.framework; name = AudioUnit.framework; path = System/Library/Frameworks/AudioUnit.framework; sourceTree = SDKROOT; };
 		ABC570D4134431DA00E7B0B1 /* OpenGL.framework */ = {isa = PBXFileReference; lastKnownFileType = wrapper.framework; name = OpenGL.framework; path = System/Library/Frameworks/OpenGL.framework; sourceTree = SDKROOT; };
 		ABC719E1138CB25E002827A9 /* DefaultKeyMappings.plist */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = text.plist.xml; path = DefaultKeyMappings.plist; sourceTree = "<group>"; };
+		ABCFA9F2178BDE920030C8BA /* encrypt.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; name = encrypt.h; path = ../encrypt.h; sourceTree = "<group>"; };
+		ABCFA9F3178BDE920030C8BA /* encrypt.cpp */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.cpp.cpp; name = encrypt.cpp; path = ../encrypt.cpp; sourceTree = "<group>"; };
 		ABD103FE1346652500AF11D1 /* cocoa_core.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = cocoa_core.h; sourceTree = "<group>"; };
 		ABD103FF1346652500AF11D1 /* cocoa_input.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = cocoa_input.h; sourceTree = "<group>"; };
 		ABD104001346652500AF11D1 /* cocoa_rom.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = cocoa_rom.h; sourceTree = "<group>"; };
@@ -1495,6 +1500,7 @@
 				ABD1FEAD1345AC8400AF11D1 /* Disassembler.cpp */,
 				ABD1FEAE1345AC8400AF11D1 /* driver.cpp */,
 				ABD1FEAF1345AC8400AF11D1 /* emufile.cpp */,
+				ABCFA9F3178BDE920030C8BA /* encrypt.cpp */,
 				ABD1FEB01345AC8400AF11D1 /* FIFO.cpp */,
 				ABD1FEB11345AC8400AF11D1 /* firmware.cpp */,
 				ABD1FEB21345AC8400AF11D1 /* fs-linux.cpp */,
@@ -1545,6 +1551,7 @@
 				ABD1FE7A1345AC8400AF11D1 /* driver.h */,
 				ABD1FE7C1345AC8400AF11D1 /* emufile.h */,
 				ABD1FE7B1345AC8400AF11D1 /* emufile_types.h */,
+				ABCFA9F2178BDE920030C8BA /* encrypt.h */,
 				ABD1FE7D1345AC8400AF11D1 /* FIFO.h */,
 				ABD1FE7E1345AC8400AF11D1 /* firmware.h */,
 				ABD1FE7F1345AC8400AF11D1 /* fs.h */,
@@ -2192,6 +2199,7 @@
 				ABD1FF0C1345AC9C00AF11D1 /* slot1_r4.cpp in Sources */,
 				ABD1FF0D1345AC9C00AF11D1 /* slot1_retail.cpp in Sources */,
 				AB6FBEF6139B6258007BB045 /* slot1_retail_nand.cpp in Sources */,
+				ABCFA9F5178BDE920030C8BA /* encrypt.cpp in Sources */,
 				ABD1FF0E1345AC9C00AF11D1 /* slot2_expMemory.cpp in Sources */,
 				ABD1FF0F1345AC9C00AF11D1 /* slot2_gbagame.cpp in Sources */,
 				ABD1FF101345AC9C00AF11D1 /* slot2_guitarGrip.cpp in Sources */,
@@ -2402,6 +2410,7 @@
 				AB796D5D15CDCBA200C59155 /* epx.cpp in Sources */,
 				AB796D5E15CDCBA200C59155 /* hq2x.cpp in Sources */,
 				AB796D5F15CDCBA200C59155 /* hq4x.cpp in Sources */,
+				ABCFA9F4178BDE920030C8BA /* encrypt.cpp in Sources */,
 				AB796D6015CDCBA200C59155 /* lq2x.cpp in Sources */,
 				AB796D6115CDCBA200C59155 /* scanline.cpp in Sources */,
 				AB796D6215CDCBA200C59155 /* coreaudiosound.cpp in Sources */,
@@ -2455,6 +2464,7 @@
 			buildActionMask = 2147483647;
 			files = (
 				ABE9EEEB1501C78700D3FB19 /* fs-linux.cpp in Sources */,
+				ABCFA9F6178BDE920030C8BA /* encrypt.cpp in Sources */,
 				ABB3C66C1501C04F00E0C22E /* NDSGameCore.mm in Sources */,
 				ABB3C6701501C04F00E0C22E /* videofilter.cpp in Sources */,
 				ABB3C6721501C04F00E0C22E /* cocoa_file.mm in Sources */,

src/encrypt.h
@@ -18,16 +18,17 @@
 #ifndef _ENCRYPT_H_
 #define _ENCRYPT_H_
 #include "types.h"
+#include <string.h>
 
 struct _KEY1
 {
-	_KEY1(u8 *keyBufPtr)
+	_KEY1(u8 *inKeyBufPtr)
 	{
 		if (keyBuf) delete keyBuf;
 		keyBuf = new u32 [0x1048 / sizeof(u32)];
 		memset(keyBuf, 0x00, 0x1048);
 		memset(&keyCode[0], 0, sizeof(keyCode));
-		this->keyBufPtr = keyBufPtr;
+		this->keyBufPtr = inKeyBufPtr;
 	}
 
 	~_KEY1()
@@ -51,8 +52,8 @@ struct _KEY1
 
 struct _KEY2
 {
-	_KEY2() :	seed0(0x58C56DE0E8U), 
-					seed1(0x5C879B9B05U)
+	_KEY2() :	seed0(0x58C56DE0E8ULL), 
+					seed1(0x5C879B9B05ULL)
 	{}
 
 	u64 seed0;