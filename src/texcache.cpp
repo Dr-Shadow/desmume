Dr-Shadow/desmume

commit a22f6121a5fdce8b92c6219f66f1c24b8efb99e9

GFX3D: - Fix texture coloring bugs with 4x4 compressed textures on big-endian systems. (This should be the last of the texture coloring bugs.) - Do small optimization to 4x4 compressed texture conversion. - Do some minor code cleanup.
git-svn-id: svn://svn.code.sf.net/p/desmume/code/trunk/desmume@5128 5577840e-afb3-4e59-9ef3-2b7f898b1952

src/texcache.cpp
@@ -399,8 +399,8 @@ public:
 		//Texture conversion
 		//============================================================================ 
 
-		const u32 opaqueColor = TEXFORMAT==TexFormat_32bpp?255:31;
-		u32 palZeroTransparent = (1-((format>>29)&1))*opaqueColor;
+		const u8 opaqueColor = (TEXFORMAT == TexFormat_32bpp) ? 0xFF : 0x1F;
+		const u8 palZeroTransparent = ( 1 - ((format>>29) & 1) ) * opaqueColor;
 
 		switch (newitem->mode)
 		{
@@ -488,15 +488,13 @@ public:
 			break;
 		case TEXMODE_4X4:
 			{
-				//RGB16TO32 is used here because the other conversion macros result in broken interpolation logic
-
 				if(ms.numItems != 1) {
 					PROGINFO("Your 4x4 texture has overrun its texture slot.\n");
 				}
 				//this check isnt necessary since the addressing is tied to the texture data which will also run out:
 				//if(msIndex.numItems != 1) PROGINFO("Your 4x4 texture index has overrun its slot.\n");
 
-	#define PAL4X4(offset) ( *(u16*)( MMU.texInfo.texPalSlot[((paletteAddress + (offset)*2)>>14)&0x7] + ((paletteAddress + (offset)*2)&0x3FFF) ) )
+	#define PAL4X4(offset) LE_TO_LOCAL_16( *(u16*)( MMU.texInfo.texPalSlot[((paletteAddress + (offset)*2)>>14)&0x7] + ((paletteAddress + (offset)*2)&0x3FFF) ) )
 
 				u16* slot1;
 				u32* map = (u32*)ms.items[0].ptr;
@@ -533,55 +531,70 @@ public:
 							continue;
 						}
 
-						u32 currBlock	= map[d];
-						u16 pal1		= slot1[d];
+						u32 currBlock	= LE_TO_LOCAL_32(map[d]);
+						u16 pal1		= LE_TO_LOCAL_16(slot1[d]);
 						u16 pal1offset	= (pal1 & 0x3FFF)<<1;
 						u8  mode		= pal1>>14;
 						u32 tmp_col[4];
 						
-						tmp_col[0]=RGB16TO32(PAL4X4(pal1offset),255);
-						tmp_col[1]=RGB16TO32(PAL4X4(pal1offset+1),255);
+						tmp_col[0] = RGB15TO32( PAL4X4(pal1offset), 0xFF );
+						tmp_col[1] = RGB15TO32( PAL4X4(pal1offset+1), 0xFF );
 
 						switch (mode) 
 						{
-						case 0:
-							tmp_col[2]=RGB16TO32(PAL4X4(pal1offset+2),255);
-							tmp_col[3]=RGB16TO32(0x7FFF,0);
-							break;
-						case 1:
-							tmp_col[2]=(((tmp_col[0]&0xFF)+(tmp_col[1]&0xff))>>1)|
-								(((tmp_col[0]&(0xFF<<8))+(tmp_col[1]&(0xFF<<8)))>>1)|
-								(((tmp_col[0]&(0xFF<<16))+(tmp_col[1]&(0xFF<<16)))>>1)|
-								(0xff<<24);
-							tmp_col[3]=RGB16TO32(0x7FFF,0);
-							break;
-						case 2:
-							tmp_col[2]=RGB16TO32(PAL4X4(pal1offset+2),255);
-							tmp_col[3]=RGB16TO32(PAL4X4(pal1offset+3),255);
-							break;
-						case 3: 
+							case 0:
+								tmp_col[2] = RGB15TO32( PAL4X4(pal1offset+2), 0xFF );
+								tmp_col[3] = RGB15TO32(0x7FFF, 0x00);
+								break;
+								
+							case 1:
+#ifdef LOCAL_BE
+								tmp_col[2]	= ( (((tmp_col[0]&0xFF000000)>>1)+((tmp_col[1]&0xFF000000)>>1)) & 0xFF000000 ) |
+											  ( ((tmp_col[0]&0x00FF0000)+(tmp_col[1]&0x00FF0000))>>1 ) |
+											  ( ((tmp_col[0]&0x0000FF00)+(tmp_col[1]&0x0000FF00))>>1 ) |
+											  0x000000FF;
+								tmp_col[3]	= 0xFFFFFF00;
+#else
+								tmp_col[2]	= ( ((tmp_col[0]&0x000000FF)+(tmp_col[1]&0x000000FF))>>1 ) |
+											  ( ((tmp_col[0]&0x0000FF00)+(tmp_col[1]&0x0000FF00))>>1 ) |
+											  ( ((tmp_col[0]&0x00FF0000)+(tmp_col[1]&0x00FF0000))>>1 ) |
+											  0xFF000000;
+								tmp_col[3]	= 0x00FFFFFF;
+#endif
+								break;
+								
+							case 2:
+								tmp_col[2] = RGB15TO32( PAL4X4(pal1offset+2), 0xFF );
+								tmp_col[3] = RGB15TO32( PAL4X4(pal1offset+3), 0xFF );
+								break;
+								
+							case 3:
 							{
-								u32 red1, red2;
-								u32 green1, green2;
-								u32 blue1, blue2;
-								u16 tmp1, tmp2;
+#ifdef LOCAL_BE
+								const u32 r0	= (tmp_col[0]>>24) & 0x000000FF;
+								const u32 r1	= (tmp_col[1]>>24) & 0x000000FF;
+								const u32 g0	= (tmp_col[0]>>16) & 0x000000FF;
+								const u32 g1	= (tmp_col[1]>>16) & 0x000000FF;
+								const u32 b0	= (tmp_col[0]>> 8) & 0x000000FF;
+								const u32 b1	= (tmp_col[1]>> 8) & 0x000000FF;
+#else
+								const u32 r0	=  tmp_col[0]      & 0x000000FF;
+								const u32 r1	=  tmp_col[1]      & 0x000000FF;
+								const u32 g0	= (tmp_col[0]>> 8) & 0x000000FF;
+								const u32 g1	= (tmp_col[1]>> 8) & 0x000000FF;
+								const u32 b0	= (tmp_col[0]>>16) & 0x000000FF;
+								const u32 b1	= (tmp_col[1]>>16) & 0x000000FF;
+#endif
 
-								red1=tmp_col[0]&0xff;
-								green1=(tmp_col[0]>>8)&0xff;
-								blue1=(tmp_col[0]>>16)&0xff;
-								red2=tmp_col[1]&0xff;
-								green2=(tmp_col[1]>>8)&0xff;
-								blue2=(tmp_col[1]>>16)&0xff;
+								const u16 tmp1	= (  (r0*5 + r1*3)>>6) |
+												  ( ((g0*5 + g1*3)>>6) <<  5 ) |
+												  ( ((b0*5 + b1*3)>>6) << 10 );
+								const u16 tmp2	= (  (r0*3 + r1*5)>>6) |
+												  ( ((g0*3 + g1*5)>>6) <<  5 ) |
+												  ( ((b0*3 + b1*5)>>6) << 10 );
 
-								tmp1=((red1*5+red2*3)>>6)|
-									(((green1*5+green2*3)>>6)<<5)|
-									(((blue1*5+blue2*3)>>6)<<10);
-								tmp2=((red2*5+red1*3)>>6)|
-									(((green2*5+green1*3)>>6)<<5)|
-									(((blue2*5+blue1*3)>>6)<<10);
-
-								tmp_col[2]=RGB16TO32(tmp1,255);
-								tmp_col[3]=RGB16TO32(tmp2,255);
+								tmp_col[2] = RGB15TO32(tmp1, 0xFF);
+								tmp_col[3] = RGB15TO32(tmp2, 0xFF);
 								break;
 							}
 						}
@@ -592,9 +605,15 @@ public:
 							{
 								tmp_col[i] >>= 2;
 								tmp_col[i] &= 0x3F3F3F3F;
+#ifdef LOCAL_BE
+								u32 a = tmp_col[i] & 0x000000FF;
+								tmp_col[i] &= 0xFFFFFF00;
+								tmp_col[i] |= (a>>1);
+#else
 								u32 a = tmp_col[i]>>24;
 								tmp_col[i] &= 0x00FFFFFF;
 								tmp_col[i] |= (a>>1)<<24;
+#endif
 							}
 						}
 

src/gfx3d.h
@@ -71,13 +71,6 @@ class EMUFILE;
 #define GFX3D_POS_TEST 0x71
 #define GFX3D_VEC_TEST 0x72
 #define GFX3D_NOP_NOARG_HACK 0xDD
-		
-//produce a 32bpp color from a DS RGB16
-#ifdef WORDS_BIGENDIAN
-	#define RGB16TO32(col,alpha) ( (alpha) | ((((col) & 0x7C00)>>7)<<8) | ((((col) & 0x03E0)>>2)<<16) | ((((col) & 0x001F)<<3)<<24) )
-#else
-	#define RGB16TO32(col,alpha) ( ((alpha)<<24) | ((((col) & 0x7C00)>>7)<<16) | ((((col) & 0x03E0)>>2)<<8) | (((col) & 0x001F)<<3) )
-#endif
 
 //produce a 32bpp color from a ds RGB15, using a table
 #define RGB15TO32_NOALPHA(col) ( color_15bit_to_24bit[col&0x7FFF] )
@@ -117,13 +110,6 @@ inline u32 RGB15TO6665(u16 col, u8 alpha5)
 
 //produce a 16bpp color from a ds RGB15, using a table
 #define RGB15TO16_REVERSE(col) ( color_15bit_to_16bit_reverse[(col)&0x7FFF] )
-
-//produce a 32bpp color from a ds RGB15 plus an 8bit alpha, not using a table (but using other tables)
-#ifdef WORDS_BIGENDIAN
-	#define RGB15TO32_DIRECT(col,alpha8) ( (alpha8) | (material_5bit_to_8bit[((col)>>10)&0x1F] << 8) | (material_5bit_to_8bit[((col)>>5)&0x1F]<<16) | (material_5bit_to_8bit[(col)&0x1F]<<24) )
-#else
-	#define RGB15TO32_DIRECT(col,alpha8) ( ((alpha8)<<24) | (material_5bit_to_8bit[((col)>>10)&0x1F]<<16) | (material_5bit_to_8bit[((col)>>5)&0x1F]<<8) | material_5bit_to_8bit[(col)&0x1F] )
-#endif
 
 //produce a 15bpp color from individual 5bit components
 #define R5G5B5TORGB15(r,g,b) ( (r) | ((g)<<5) | ((b)<<10) )

commit 21bc2db737bdc9d6437eda9cf813919def733731

GFX3D: - Fix bug where 4x4 compressed textures in Mode 2 would mix the incorrect colors. (Regression from r5128.) - Do some minor code cleanup.
git-svn-id: svn://svn.code.sf.net/p/desmume/code/trunk/desmume@5129 5577840e-afb3-4e59-9ef3-2b7f898b1952

src/texcache.cpp
@@ -549,15 +549,14 @@ public:
 								
 							case 1:
 #ifdef LOCAL_BE
-								tmp_col[2]	= ( (((tmp_col[0]&0xFF000000)>>1)+((tmp_col[1]&0xFF000000)>>1)) & 0xFF000000 ) |
-											  ( ((tmp_col[0]&0x00FF0000)+(tmp_col[1]&0x00FF0000))>>1 ) |
-											  ( ((tmp_col[0]&0x0000FF00)+(tmp_col[1]&0x0000FF00))>>1 ) |
+								tmp_col[2]	= ( (((tmp_col[0] & 0xFF000000) >> 1)+((tmp_col[1] & 0xFF000000)  >> 1)) & 0xFF000000 ) |
+											  ( (((tmp_col[0] & 0x00FF0000)      + (tmp_col[1] & 0x00FF0000)) >> 1)  & 0x00FF0000 ) |
+											  ( (((tmp_col[0] & 0x0000FF00)      + (tmp_col[1] & 0x0000FF00)) >> 1)  & 0x0000FF00 ) |
 											  0x000000FF;
 								tmp_col[3]	= 0xFFFFFF00;
 #else
-								tmp_col[2]	= ( ((tmp_col[0]&0x000000FF)+(tmp_col[1]&0x000000FF))>>1 ) |
-											  ( ((tmp_col[0]&0x0000FF00)+(tmp_col[1]&0x0000FF00))>>1 ) |
-											  ( ((tmp_col[0]&0x00FF0000)+(tmp_col[1]&0x00FF0000))>>1 ) |
+								tmp_col[2]	= ( (((tmp_col[0] & 0x00FF00FF) + (tmp_col[1] & 0x00FF00FF)) >> 1) & 0x00FF00FF ) |
+											  ( (((tmp_col[0] & 0x0000FF00) + (tmp_col[1] & 0x0000FF00)) >> 1) & 0x0000FF00 ) |
 											  0xFF000000;
 								tmp_col[3]	= 0x00FFFFFF;
 #endif
@@ -601,18 +600,18 @@ public:
 
 						if(TEXFORMAT==TexFormat_15bpp)
 						{
-							for(int i=0;i<4;i++)
+							for (size_t i = 0; i < 4; i++)
 							{
-								tmp_col[i] >>= 2;
-								tmp_col[i] &= 0x3F3F3F3F;
 #ifdef LOCAL_BE
-								u32 a = tmp_col[i] & 0x000000FF;
-								tmp_col[i] &= 0xFFFFFF00;
-								tmp_col[i] |= (a>>1);
+								const u32 a = (tmp_col[i] >> 3) & 0x0000001F;
+								tmp_col[i] >>= 2;
+								tmp_col[i] &= 0x3F3F3F00;
+								tmp_col[i] |= a;
 #else
-								u32 a = tmp_col[i]>>24;
-								tmp_col[i] &= 0x00FFFFFF;
-								tmp_col[i] |= (a>>1)<<24;
+								const u32 a = (tmp_col[i] >> 3) & 0x1F000000;
+								tmp_col[i] >>= 2;
+								tmp_col[i] &= 0x003F3F3F;
+								tmp_col[i] |= a;
 #endif
 							}
 						}
@@ -620,23 +619,19 @@ public:
 						//TODO - this could be more precise for 32bpp mode (run it through the color separation table)
 
 						//set all 16 texels
-						for (int sy = 0; sy < 4; sy++)
+						for (size_t sy = 0; sy < 4; sy++)
 						{
 							// Texture offset
 							u32 currentPos = (x<<2) + tmpPos[sy];
 							u8 currRow = (u8)((currBlock>>(sy<<3))&0xFF);
 
-							dwdst[currentPos] = tmp_col[currRow&3];
+							dwdst[currentPos  ] = tmp_col[ currRow    &3];
 							dwdst[currentPos+1] = tmp_col[(currRow>>2)&3];
 							dwdst[currentPos+2] = tmp_col[(currRow>>4)&3];
 							dwdst[currentPos+3] = tmp_col[(currRow>>6)&3];
 						}
-
-
 					}
 				}
-
-
 				break;
 			}
 		case TEXMODE_A5I3: